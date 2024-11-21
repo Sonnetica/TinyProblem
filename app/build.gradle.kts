b5c8a9ad4bc2cdc38a07987c6da797d755f26729
Chris <chrischua02@gmail.com>
2024-11-22 00:24:56 +0800

    Added Bluetooth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,13 +40,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.firestore)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
